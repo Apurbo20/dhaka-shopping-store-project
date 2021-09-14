863eda2b67c27832e9bc7213cfb059417cab1064
Md. Saif Ali <57108914+Apurbo20@users.noreply.github.com>
2021-09-14 20:23:43 +0600

    Add files via upload

---
+++ ranga-store-fix-Apurbo20-main/js/app.js
@@ -0,0 +1,125 @@
+//load products
+const loadProducts = () => {
+  const url = `https://fakestoreapi.com/products`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => showProducts(data));
+};
+//call for load products
+loadProducts();
+
+//show all product in UI 
+const showProducts = (products) => {
+  const allProducts = products.map((pd) => pd);
+  console.log(allProducts);
+  for (const product of allProducts) {
+    const image = product.image;
+    const div = document.createElement("div");
+    div.classList.add("product");
+    div.innerHTML = `
+    <div  class="single-product background_color">
+      <div>
+        <img class="product-image" src=${image}></img>
+      </div>
+      <h4>${product.title}</h4>
+      <p>Category: ${product.category}</p>
+      <h3>Price: $ ${product.price}</h3>
+      <p>Rating: ${product.rating.rate}</p>
+      <p>Reviews: ${product.rating.count}</p>  
+      <div>               
+        <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-warning mx-5">Add to cart</button>
+        <button id="details-btn" class="btn btn-secondary" onclick="loadDetails(${product.id})">Details</button>
+      </div>
+    </div>
+    `;
+    document.getElementById("all-products").appendChild(div);
+  }
+};
+//adding to cart
+let count = 0;
+const addToCart = (id, price) => {
+  count = count + 1;
+  updatePrice("price", price);
+  updateTaxAndCharge();
+  updateTotal();
+  document.getElementById("total-Products").innerText = count;
+};
+//extract values
+const getInputValue = (id) => {
+  const element = document.getElementById(id).innerText;
+  const converted = parseFloat(element);
+  return converted;
+};
+
+//main price update function
+const updatePrice = (id, value) => {
+  const convertedOldPrice = getInputValue(id);
+  const convertPrice = parseFloat(value);
+  const total = convertedOldPrice + convertPrice;
+  document.getElementById(id).innerText = total.toFixed(2);
+};
+
+//set innerText function
+const setInnerText = (id, value) => {
+  value = parseFloat(value).toFixed(2);
+  document.getElementById(id).innerText = value;
+};
+
+//update delivery charge and total Tax
+const updateTaxAndCharge = () => {
+  const priceConverted = getInputValue("price");
+  if (priceConverted > 200) {
+    setInnerText("delivery-charge", 30);
+    setInnerText("total-tax", priceConverted * 0.2);
+  }
+  if (priceConverted > 400) {
+    setInnerText("delivery-charge", 50);
+    setInnerText("total-tax", priceConverted * 0.3);
+  }
+  if (priceConverted > 500) {
+    setInnerText("delivery-charge", 60);
+    setInnerText("total-tax", priceConverted * 0.4);
+  }
+};
+
+//grandTotal update function
+const updateTotal = () => {
+  const grandTotal =
+    getInputValue("price") + getInputValue("delivery-charge") +
+    getInputValue("total-tax");
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
+};
+
+//load single product
+const loadDetails = id => {
+  const url = `https://fakestoreapi.com/products/${id}`
+  fetch(url)
+    .then(res => res.json())
+    .then(data => showDetails(data));
+};
+//show single product detail
+const showDetails = product => {
+  const detailDiv = document.getElementById('show-details');
+  detailDiv.innerHTML = `
+  <div class="single-product product-details background_color">
+    <div>
+      <img class="product-image" src=${product.image}></img>
+    </div>
+    <h3>${product.title}</h3>
+    <p>Description: ${product.description}</p>
+    <p>Category: ${product.category}</p>
+    <h3>Price: $ ${product.price}</h3>
+    <p>Rating: ${product.rating.rate}</p>
+    <p>Reviews: ${product.rating.count}</p>  
+    <div>               
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-warning mx-5">Add to cart</button>
+      <button id="details-btn" class="btn btn-danger" onclick="hideDiv()">Cancel</button>
+    </div>
+  </div>
+`;
+  detailDiv.style.display = 'flex';
+};
+//hiding single product div
+const hideDiv = () => {
+  document.getElementById('show-details').style.display = 'none';
+}
